144e58694ccfa58370b4683e2c5fdb60382097be
Piyush Mishra <pmishra70039@gmail.com>
2026-06-05 21:29:53 +0530

    AUTH ROUTES

--- backend5/server.js
+++ backend5/server.js
@@ -1,81 +1,199 @@
-const express = require("express")
-require("./db")
-const product = require("./models/Product")
-const app = express()
+const express = require("express");
+require("./db");
 
-app.use(express.json())
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-app.get("/product", async (req,res) => {   // to get all product
+const Product = require("./models/Product");
+const User = require("./models/User");
+
+const app = express();
+
+app.use(express.json());
+
+
+// ================= PRODUCT ROUTES =================
+
+// Get All Products
+app.get("/product", async (req, res) => {
     try {
-        const products = await product.find()
-        res.json(products)
+        const products = await Product.find();
+        res.json(products);
     } catch (err) {
         res.status(500).json({
-            message : err.message
-        })
+            message: err.message
+        });
     }
-})
+});
 
-app.post("/product", async (req,res) => {    // to add the product
+
+// Add Product
+app.post("/product", async (req, res) => {
     try {
-        const Product = await product.create(req.body)
+        const product = await Product.create(req.body);
 
         res.json({
-            message: "product added",
-            Product
-        })
+            message: "Product Added",
+            product
+        });
+
     } catch (err) {
         res.status(500).json({
-            message : err.message
-        })
+            message: err.message
+        });
     }
-})
+});
 
 
-app.put("/product/:id", async (req,res) => {    // to update the product 
+// Update Product
+app.put("/product/:id", async (req, res) => {
     try {
-        const Product = await product.findByIdAndUpdate(
+
+        const product = await Product.findByIdAndUpdate(
             req.params.id,
             req.body,
-            {new: true}
-        )
-        if (!Product) {
+            { new: true }
+        );
+
+        if (!product) {
             return res.status(404).json({
-                message: "product not found"
-            })
+                message: "Product not found"
+            });
         }
+
         res.json({
-            message : "producted updated",
-            Product
-        })
+            message: "Product Updated",
+            product
+        });
+
     } catch (err) {
         res.status(500).json({
             message: err.message
-        })
+        });
     }
-})
+});
 
-app.delete("/product/:id" , async (req,res)=>{   // to delete the product 
+
+// Delete Product
+app.delete("/product/:id", async (req, res) => {
     try {
-        const Product = await product.findByIdAndDelete(
+
+        const product = await Product.findByIdAndDelete(
             req.params.id
-        )
-        if (!Product) {
+        );
+
+        if (!product) {
             return res.status(404).json({
-                message : "product not found"
-            })
+                message: "Product not found"
+            });
         }
+
         res.json({
-            message : "product Deleted" 
-        })
+            message: "Product Deleted"
+        });
+
     } catch (err) {
         res.status(500).json({
             message: err.message
-        })
+        });
     }
-})
+});
 
-app.listen(5000,()=>{
-    console.log("Server runing on port 5000");
-    
-})
+
+// ================= AUTH ROUTES =================
+
+// Register
+app.post("/register", async (req, res) => {
+    try {
+
+        const { name, email, password } = req.body;
+
+        const userExists = await User.findOne({ email });
+
+        if (userExists) {
+            return res.status(400).json({
+                message: "User already exists"
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(
+            password,
+            10
+        );
+
+        const newUser = await User.create({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        res.json({
+            message: "User Registered Successfully",
+            user: newUser
+        });
+
+    } catch (err) {
+        console.log(err);
+
+        res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
+
+// Login
+app.post("/login", async (req, res) => {
+    try {
+
+        const { email, password } = req.body;
+
+        const foundUser = await User.findOne({ email });
+
+        if (!foundUser) {
+            return res.status(400).json({
+                message: "Invalid Email"
+            });
+        }
+
+        const isMatch = await bcrypt.compare(
+            password,
+            foundUser.password
+        );
+
+        if (!isMatch) {
+            return res.status(400).json({
+                message: "Invalid Password"
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                id: foundUser._id
+            },
+            "mysecretkey",
+            {
+                expiresIn: "1d"
+            }
+        );
+
+        res.json({
+            message: "Login Successfully",
+            token
+        });
+
+    } catch (err) {
+        console.log(err);
+
+        res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
+
+// ================= SERVER =================
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
